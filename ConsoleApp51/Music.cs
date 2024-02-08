89a3d044d348f1aa95bfc7609a828e229ab0bc63
chekPec <dydnikvn@gmail.com>
2024-02-08 18:52:58 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp51/Music.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp51
+{
+    public class Music
+    {
+        public string MusicName {  get; set; }
+
+        public int Id {  get; set; }
+
+        public string Memory { get; private set; }
+
+        public MyDate Date { get; private set; }
+    }
+
+
+
+}
---
+++ ConsoleApp51/Program.cs
@@ -0,0 +1,61 @@
+﻿using ConsoleApp51;
+
+void PrintGreen(string message)
+{
+    ConsoleColor color = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(message);
+    Console.ForegroundColor = color;
+}
+void PrintPurple(string message)
+{
+    ConsoleColor color = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(message);
+    Console.ForegroundColor = color;
+}
+void PrintBlue(string message)
+{
+    ConsoleColor color = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(message);
+    Console.ForegroundColor = color;
+}
+
+void PrintRed(string message)
+{
+    ConsoleColor color = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(message);
+    Console.ForegroundColor = color;
+}
+PrintGreen("Привет, я программа которая расскажет тебе сколько ты слушаешь музыки и твою статистику");
+PrintGreen("Введите номер команды");
+MainServer server;
+
+try
+{
+    server = new MainServer();
+}
+catch (Exception ex)
+{
+    PrintRed(ex.Message);
+    Console.ReadKey();
+    return;
+}
+
+while (true)
+{
+   
+    switch (Console.ReadLine())
+    {
+        case "1": 
+
+
+
+
+
+
+
+    }
+}
